03cf38c7daebb6862ba76a0c21fc308d68b1f801
Luis Calle Lazo <callelazodeynarluis@gmail.com>
2024-08-31 13:20:53 -0400

    Storage image in server

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Image;
 
 class ImageController extends Controller
 {
     public function store(Request $request)
     {
         $image = $request->file('file');
+
+        $name = Str::uuid().".".$image->extension();
+        $imageServer = Image::make($image);
+
+        $imageServer->fit(1000, 1000);
+        $imagePath = public_path('uploads') . '/' . $name;
+
+        $imageServer->save($imagePath);
 
         return response()->json(
             [
